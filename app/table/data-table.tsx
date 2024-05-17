b2ee4c1ae4b9dfb3b0722973d800b5425e467127
minhanPark <alsgks1218@naver.com>
2024-05-17 18:00:48 +0900

    .

---
+++ app/table/data-table.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
