4ca59804242912d6d07b08bedbdff7ec255e29ee
minhanPark <alsgks1218@naver.com>
2024-05-20 17:58:12 +0900

    table visibility까지 테스트

--- app/table/columns.tsx
+++ app/table/columns.tsx
@@ -1,6 +1,17 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowDown, MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export type Payment = {
   id: string;
@@ -16,10 +27,62 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: "email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "amount",
-    header: "Amount",
+    // 만약 헤더에 줘야할 커스텀이 있다면 header를 사용해 줄 수 있다.
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return (
+        <div className="text-right font-medium">
+          <span className="inline-block bg-green-400 rounded-full p-1">
+            {formatted}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem onClick={() => console.log(payment)}>
+              Payment details
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,7 +1,89 @@
-export default function Page() {
+import { Payment, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Payment[]> {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52d",
+      amount: 120,
+      status: "success",
+      email: "m2@example.com",
+    },
+    {
+      id: "728ed52e",
+      amount: 130,
+      status: "failed",
+      email: "m3@example.com",
+    },
+    {
+      id: "728ed52h",
+      amount: 150,
+      status: "pending",
+      email: "m4@example.com",
+    },
+    {
+      id: "728ed52fr",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52de",
+      amount: 120,
+      status: "success",
+      email: "m2@example.com",
+    },
+    {
+      id: "728ed52ew",
+      amount: 130,
+      status: "failed",
+      email: "m3@example.com",
+    },
+    {
+      id: "728ed52hq",
+      amount: 150,
+      status: "pending",
+      email: "m4@example.com",
+    },
+    {
+      id: "728ed52fa",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52ds",
+      amount: 120,
+      status: "success",
+      email: "m2@example.com",
+    },
+    {
+      id: "728ed52ed",
+      amount: 130,
+      status: "failed",
+      email: "m3@example.com",
+    },
+    {
+      id: "728ed52hf",
+      amount: 150,
+      status: "pending",
+      email: "m4@example.com",
+    },
+  ];
+}
+
+export default async function Page() {
+  const data = await getData();
+
   return (
-    <div className="">
-      <h3>테이블 페이지</h3>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
